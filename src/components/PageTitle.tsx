45ac2320b1ec3b9526b9163c9a3d673aece6b643
chaiit2199 <chaiit2199@gmail.com>
2025-11-01 18:51:03 +0700

    update

--- src/components/PageTitle.tsx
+++ src/components/PageTitle.tsx
@@ -14,7 +14,7 @@ export default function PageTitle() {
           spaceBetween={20}
           slidesPerView={1}
           loop={true}
-          autoplay={{ delay: 4000 }}
+          // autoplay={{ delay: 4000 }}
           effect="fade"
           fadeEffect={{ crossFade: true }}
           modules={[Autoplay, EffectFade]}
@@ -22,21 +22,31 @@ export default function PageTitle() {
         <SwiperSlide>
           <div className="slide-item">
             <div className="slide-item--content container">
-              <h2 className="slide-item--title">Fall Winter Collection</h2>
-              <p className="slide-item--sub-title">Vivamus lacinia odio vitae vestibulum vestibulum</p>
-              <div className="slide-item--action"><a className="cs-btn" href="/shop-default-list">Shop now</a></div>
+              <h2 className="slide-item--title">Tin nóng bóng đá 24/7 - Cập nhật từng giây!</h2>
+              <p className="slide-item--sub-title">Theo dõi tỉ số trực tiếp, kết quả trận đấu và tin tức nóng hổi từ khắp các giải đấu lớn: Premier League, Champions League, La Liga và hơn thế nữa.</p>
+              <div className="slide-item--action"><a className="cs-btn text-black bg-white" href="/shop-default-list">Shop now</a></div>
             </div>
-            <img src="/images/banner/banner-1.jpg" alt="Slide Image" className="w-full"/>
+            <img src="/images/banner/banner-1.jpg" alt="Slide Image" className="slide-item--backgroup"/>
           </div>
         </SwiperSlide>
         <SwiperSlide>
           <div className="slide-item">
             <div className="slide-item--content container"> 
-              <h2 className="slide-item--title">Spring Summer  Collection</h2>
-              <p className="slide-item--sub-title">Discover the elegance of renewal with soft tones and flowing textures.</p>
-              <div className="slide-item--action"><a className="cs-btn" href="/shop-default-list">Shop now</a></div>
+              <h2 className="slide-item--title">Chiến thuật, phân tích & nhận định từ chuyên gia!</h2>
+              <p className="slide-item--sub-title">Hiểu sâu hơn về từng trận đấu – chiến thuật, phong độ cầu thủ và dự đoán kết quả từ những cây viết giàu kinh nghiệm.</p>
+              <div className="slide-item--action"><a className="cs-btn text-black bg-white" href="/shop-default-list">Shop now</a></div>
             </div>
-            <img src="/images/banner/banner-2.jpg" alt="Slide Image" className="w-full"/>
+            <img src="/images/banner/banner-2.png" alt="Slide Image" className="slide-item--backgroup"/>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="slide-item">
+            <div className="slide-item--content container"> 
+              <h2 className="slide-item--title">Nơi cảm xúc bóng đá lan tỏa!</h2>
+              <p className="slide-item--sub-title">Chia sẻ cảm xúc, bình luận sau trận, và hòa mình cùng cộng đồng fan cuồng nhiệt – nơi mọi người đều yêu bóng đá như bạn.</p>
+              <div className="slide-item--action"><a className="cs-btn text-black bg-white" href="/shop-default-list">Shop now</a></div>
+            </div>
+            <img src="/images/banner/banner-3.png" alt="Slide Image" className="slide-item--backgroup"/>
           </div>
         </SwiperSlide>
       </Swiper>
